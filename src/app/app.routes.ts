fd07b05aed5bed1a6b46bfbfb9258158a558fcb3
cofi420 <filip.vidakovic.04@gmail.com>
2024-11-18 13:18:23 +0100

    Add NotificationsComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './layout/home/home.component';
 import { ChatComponent } from './layout/chat/chat.component';
 import { ProfileComponent } from './layout/profile/profile.component';
+import { NotificationsComponent } from './layout/notifications/notifications.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'chat', component: ChatComponent},
     {path: 'profile', component: ProfileComponent},
+    {path: 'notifications', component: NotificationsComponent},
 ];
