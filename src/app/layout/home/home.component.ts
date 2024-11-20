a8a1c6f5877c85be45290359dbabaab44181c2a8
Filip Tot <filipsombor@gmail.com>
2024-11-20 22:10:56 +0100

    Add cards for events and serviceProducts

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,15 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    MatSidenavModule,
+    MatSidenavModule, MatCardModule, MatButtonModule, CommonModule
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  topEvents: Event[] = [];
+  longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor`;
 }
