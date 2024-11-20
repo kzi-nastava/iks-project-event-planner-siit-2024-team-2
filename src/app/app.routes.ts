2eb6a2ed697c552b4f45bbbf3e90c0d36848fbdd
cofi420 <filip.vidakovic.04@gmail.com>
2024-11-20 19:27:47 +0100

    Add register and login components alongside new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { HomeComponent } from './layout/home/home.component';
 import { ChatComponent } from './layout/chat/chat.component';
 import { ProfileComponent } from './layout/profile/profile.component';
 import { NotificationsComponent } from './layout/notifications/notifications.component';
+import { LoginComponent } from './layout/auth/login/login.component';
+import { RegisterComponent } from './layout/auth/register/register.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
     {path: 'chat', component: ChatComponent},
     {path: 'profile', component: ProfileComponent},
     {path: 'notifications', component: NotificationsComponent},
+    {path: 'signin', component: LoginComponent},
+    {path: 'signup', component: RegisterComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: '**', redirectTo: 'home'}
 ];
---
+++ src/app/layout/auth/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  standalone: true,
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    RouterModule
+  ]
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
+}
