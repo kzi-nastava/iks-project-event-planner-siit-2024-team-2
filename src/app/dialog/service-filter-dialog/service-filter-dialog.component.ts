64755622d47bd61e99865e2303761affe15daec7
Matija Šeremet <matija.sch03@gmail.com>
2025-06-20 00:29:37 +0200

    Add all filter categories styled by scss

--- src/app/dialog/service-filter-dialog/service-filter-dialog.component.ts
+++ src/app/dialog/service-filter-dialog/service-filter-dialog.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatCheckbox } from '@angular/material/checkbox';
-import { MatDialogActions, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogContent, MatDialogRef } from '@angular/material/dialog';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-service-filter-dialog',
   standalone: true,
-  imports: [MatDialogContent, MatDialogActions, MatCheckbox],
+  imports: [MatDialogContent, MatCheckbox, ReactiveFormsModule, CommonModule],
   templateUrl: './service-filter-dialog.component.html',
   styleUrl: './service-filter-dialog.component.css'
 })
@@ -14,4 +16,10 @@ export class ServiceFilterDialogComponent {
   close() {
     this.dialogRef.close();
   }
+
+  categories = ['Music', 'Catering', 'Waiter service'];
+  selectedCategories = new FormControl([]);
+
+  eventTypes: string[] = ['Wedding', 'Funeral', 'Birthday', 'Conference'];
+  selectedEventTypes = new FormControl([]);
 }
