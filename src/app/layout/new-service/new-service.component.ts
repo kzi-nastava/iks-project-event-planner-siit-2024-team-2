eb15b72fb1b525692cad5a9fdbda1c5938e70ca9
Matija Šeremet <matija.sch03@gmail.com>
2025-07-22 01:01:33 +0200

    Add new service form validation and new forms

--- src/app/layout/new-service/new-service.component.ts
+++ src/app/layout/new-service/new-service.component.ts
@@ -1,7 +1,9 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { AbstractControl, FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { Service } from '../../model/service';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
@@ -14,21 +16,49 @@ import { forkJoin } from 'rxjs';
 @Component({
   selector: 'app-new-service',
   standalone: true,
-  imports: [FormsModule, CommonModule, MatSelectModule],
+  imports: [FormsModule, CommonModule, MatSelectModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './new-service.component.html',
   styleUrl: './new-service.component.css'
 })
 export class NewServiceComponent {
+
+  newServiceForm = new FormGroup({
+      category: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      specifies: new FormControl('', [Validators.required]),
+      price: new FormControl(0, [Validators.required, Validators.min(0)]),
+      discount: new FormControl(0, [Validators.min(0)]),
+      reservationDaysDeadline: new FormControl(0, [Validators.required, Validators.min(0)]),
+      availableEventTypes: new FormArray([]),
+      images: new FormControl('', [Validators.required]),
+      visible: new FormControl(false),
+      available: new FormControl(false),
+      automaticReserved: new FormControl(false),
+      durationForm: new FormGroup({
+        duration: new FormControl(0, [Validators.min(0)]),
+        minEngagementDuration: new FormControl(0, [Validators.min(0)]),
+        maxEngagementDuration: new FormControl(0, [Validators.min(0)]),
+      }, {validators: this.oneDurationRequiredValidator}),
+      cancellationDaysDeadline: new FormControl(0, [Validators.required, Validators.min(0)]),
+    });
+
+  oneDurationRequiredValidator(group: AbstractControl): ValidationErrors | null {
+    const duration = group.get('duration')?.value;
+    const minEngagement = group.get('minEngagementDuration')?.value;
+    const maxEngagement = group.get('maxEngagementDuration')?.value;
+
+    return (duration > 0 || (minEngagement > 0 && maxEngagement > 0)) ? null : { oneDurationRequiredValidator: true };
+  }
   serviceCategories : string[] = [];
   eventTypes: string[] = [];
-  selectedEvents: { [key: string]: boolean } = {};  // default all false
   
   // binding to the service data, on which the user clicked
   service: Service = new Service();
 
   constructor(private route: ActivatedRoute, private router: Router, private serviceService: ServiceService,
     private SPCategoryService: ServiceProductCategoryService, private eventTypeService: EventTypeService) {
-    // Initialize selectedEvents with default values
+    
     eventTypeService.getAll().subscribe(allTypes => {
       this.eventTypes = allTypes.map(t => t.name);
     })
@@ -47,9 +77,7 @@ export class NewServiceComponent {
           this.serviceCategories = allCategories.map(c => c.name);
         })
         this.service = new Service();
-        this.eventTypes.forEach((event) => {
-          this.selectedEvents[event] = false;
-        });
+        // reset all checkboxes to false
       }
     });
   }
@@ -64,10 +92,28 @@ export class NewServiceComponent {
       this.serviceCategories = categories.map(c => c.name);
 
       // waits for service and serviceCategories assigning
-      this.service.availableEventTypes.forEach((event) => {
-        this.selectedEvents[event.name] = true;
-      });
+      // this.service.availableEventTypes.forEach((type) => {
+      //   this.availableEventTypes[type.name] = true;
+      // });
     });
+  }
+
+  onCheckboxChange(event: any, eventType: string) {
+    const selectedEvents = this.newServiceForm.get('availableEventTypes') as FormArray;
+    if (event.target.checked) {
+      selectedEvents.push(new FormControl(eventType));
+    } else {
+      const index = selectedEvents.controls.findIndex(ctrl => ctrl.value === eventType);
+      if (index !== -1) {
+        selectedEvents.removeAt(index);
+      }
+    }
+  }
+
+  onSubmit() {
+    if (this.newServiceForm.valid) {
+      console.log("The service created successfully.");
+    }
   }
 
   backToAllServicesPerhaps(): void {
