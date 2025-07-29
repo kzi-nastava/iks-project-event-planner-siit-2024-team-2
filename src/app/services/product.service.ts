af94e6b6e66ee13fad796483d2536f542af5f33b
filipvidakovic <filip.vidakovic.04@gmail.com>
2025-07-29 15:15:36 +0200

    Resolve comments#

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { PagedResponse } from '../shared/model/paged-response.model';
 import { Product } from '../model/product';
 
 @Injectable({
