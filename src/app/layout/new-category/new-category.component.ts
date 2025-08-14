e91128bb1d6679e8640357689492ab1b4849a151
Matija Šeremet <matija.sch03@gmail.com>
2025-08-14 20:56:51 +0200

    Fix admin nav bar

---
+++ src/app/layout/new-category/new-category.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-category',
+  standalone: true,
+  templateUrl: './new-category.component.html',
+  styleUrl: './new-category.component.css'
+})
+export class NewCategoryComponent {
+
+}
