c9b6aa50594367c614a7dcbe5b8bf22b6e01807e
FilipT03 <filipsombor@gmail.com>
2025-08-30 19:08:59 +0200

    Refactor other files

--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,9 @@
 import 'zone.js/testing';
-import { getTestBed, TestBed } from '@angular/core/testing';
+import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { provideRouter } from '@angular/router';
 import { SharedTestingModule } from './testing/shared-testing.module';
 
 declare const require: {
