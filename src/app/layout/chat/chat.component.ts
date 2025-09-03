40e2bc66bae6b01e932e7e3acdb8f7fd4058bdfc
Matija Šeremet <matija.sch03@gmail.com>
2025-09-03 22:11:26 +0200

    Add chat sidebar

--- src/app/layout/chat/chat.component.ts
+++ src/app/layout/chat/chat.component.ts
@@ -9,11 +9,15 @@ import { Chat } from '../../model/communication/chat';
 import { FormsModule } from '@angular/forms';
 import { ChatMessageDto } from '../../services/dtos/communication/chat-message.dto';
 import { ChatMessageService } from '../../services/communication/chat-message.service';
+import { ChatMessage } from '../../model/communication/chat-message';
+import { MatButtonModule } from '@angular/material/button';
+import { User } from '../../model/user/user';
+
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [ MatSidenavModule, CommonModule, FormsModule ],
+  imports: [ MatSidenavModule, CommonModule, FormsModule, MatButtonModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
@@ -24,28 +28,47 @@ export class ChatComponent {
 
   myChats: Chat[] = [];
   chatFriendId: number | null = null;
+  chatFriend: User | null = null;
   chat: Chat | null = null;
   newMessage = '';
+  myId: Number = Number(localStorage.getItem('userId'))
 
   ngOnInit(): void {
-    this.chatFriendId = this.userContextService.getUserId();
-    if (this.chatFriendId == null) {
-      console.error('ChatComponent: userId is null');
-      return
-    }
-    this.chatService.getMineUser2Id(this.chatFriendId).subscribe(chat => {
-      if (!chat) {
-        const chat: ChatDto = {
-          toId: Number(this.chatFriendId),
-          messageIds: [],
-          status: ChatStatus.ALL_SEEN
-        }
-        this.chatService.add(chat).subscribe(chat => {
-          this.chat = chat;
-        });
-      }
-      else this.chat = chat;
+    this.chatService.getAllMyChats({ page: 0, size: 10 }).subscribe(response => {
+      this.myChats = response.content;
     });
+
+    // With whom are you chatting?
+    this.chatFriendId = this.userContextService.getUserId();
+    // 1. with SPP or EO who you found on event- or sp-details page
+    if (this.chatFriendId != null) {
+      this.chatService.getMineUser2Id(this.chatFriendId).subscribe(chat => {
+        if (!chat) {
+          const chat: ChatDto = {
+            toId: Number(this.chatFriendId),
+            messageIds: [],
+            status: ChatStatus.ALL_SEEN
+          }
+          this.chatService.add(chat).subscribe(chat => {
+            this.chat = chat;
+          });
+        }
+        else this.chat = chat;
+      });
+    }
+  }
+  // 2. with someone you've chatted with before (you found them on chat sidebar)
+  selectChat(chat: Chat) {
+    this.chat = chat;
+    // find a user who is chatting with you
+    if (chat.user1.id === this.myId) {
+      this.chatFriend = chat.user2;
+      this.chatFriendId = chat.user2.id;
+    }
+    else {
+      this.chatFriend = chat.user1;
+      this.chatFriendId = chat.user1.id;
+    }
   }
 
   sendMessage() {
